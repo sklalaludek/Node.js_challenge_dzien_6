fbe088701eba423a5b9ca50e6e67ee97b10f7b8a
sklalaludek <pattiegolis@gmail.com>
2018-02-11 00:02:04 +0100

    ex 1-2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,24 @@
 //Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static('./public/zadanie01/'));
+app.use(bodyParser.urlencoded());
+
+app.post('/division', (req, res) => {
+    const {
+        dividend,
+        divisor
+    } = req.body;
+    let result = Number(dividend) % Number(divisor);
+    if (result === 0) {
+        res.send(`${divisor} jest dzielnikiem liczby ${dividend}`);
+    } else {
+        res.send(`${divisor} nie jest dzielnikiem liczby ${dividend}`);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,34 @@
 //Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+app.use(express.static('./public/zadanie02/'));
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+app.post('/cookie/set', (req, res) => {
+    const name = req.body.name;
+    res.cookie('name', name, {
+        maxAge: 2592000000
+    });
+    res.send(`Zapisano imię ${name} w cookie`)
+});
+
+app.get('/cookie/show', (req, res) => {
+    const cookie = req.cookies.name;
+    res.send(cookie);
+});
+
+app.get('/cookie/check', (req, res) => {
+    if (req.cookies.name === undefined) {
+        res.send('Imię zostało zapisane')
+    } else {
+        res.send('Imię nie zostało zapisane w ciastku!')
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
